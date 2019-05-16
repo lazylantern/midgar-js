97156655a696ea89d96b15a5edccb10a5e7881e7
Guillaume Lachaud <guillaume.lachaud@gmail.com>
2019-05-16 10:33:29 +0200

    Completed implementation

--- midgar.js
+++ midgar.js
@@ -1,5 +1,5 @@
 class Event {
-    constructor(screen, source, type, date){
+    constructor(screen, source, type, date) {
         this.screen = screen;
         this.source = source;
         this.type = type;
@@ -10,24 +10,24 @@ class Event {
 class MidgarApi {
     static BASE_URL = "http://midgar-flask.herokuapp.com/api"
 
-    constructor(appId){
+    constructor(appId) {
         this.appId = appId
     }
 
-    checkAppIsEnabled(){
+    checkAppIsEnabled() {
         return this.postRequest("/apps/kill", {
-            app_token:this.appId
+            app_token: this.appId
         })
     }
 
-    uploadBatch(events){
+    uploadBatch(events) {
         return this.postRequest("/events", {
-            app_token:this.appId,
+            app_token: this.appId,
             events: events
         })
     }
 
-    postRequest(endpoint, params){
+    postRequest(endpoint, params) {
         const url = MidgarApi.BASE_URL + endpoint
         const body = params
         const headers = {
@@ -47,46 +47,73 @@ class MidgarApi {
 
 class MidgarManager {
 
+    static MAX_UPLOAD_BATCH_SIZE = 10
+    static UPLOAD_PERIOD_MS = 60 * 1000
+
     events = [];
     hasBeenRemotelyEnabled = false;
 
-    constructor(appId){
+    constructor(appId) {
         this.appId = appId
     }
 
-    start(){
+    start() {
         this.api = new MidgarApi(this.appId)
+        this.api.checkAppIsEnabled().then(
+            response => {
+                if (response.ok) {
+                    this.hasBeenRemotelyEnabled = true
+                    this.timerId = setInterval(this.processBatch, MidgarManager.UPLOAD_PERIOD_MS)
+                }
+            }
+        )
     }
 
-    stop(){
+    stop() {
         this.hasBeenRemotelyEnabled = false
+        clearInterval(this.timerId)
         this.events = []
     }
 
-    trackScreen(screen){
-        if(this.hasBeenRemotelyEnabled){
+    trackScreen(screen) {
+        if (this.hasBeenRemotelyEnabled) {
             this.events.push(this.createEvent(screen))
         }
     }
 
-    createEvent(screen){
+    createEvent(screen) {
         return new Event(screen, "js", "impression", new Date().getTime())
+    }
+
+    processBatch() {
+        var batch = this.events.splice(MidgarManager.MAX_UPLOAD_BATCH_SIZE)
+        this.api.uploadBatch(batch).then(
+            response => {
+                if (_DEV_) {
+                    if (response.ok) {
+                        console.log("Events successfully uploaded")
+                    } else {
+                        console.log("Something went wrong. Events got dropped.")
+                    }
+                }
+            }
+        )
     }
 
 }
 
 export default class MidgarTracker {
 
-    init(appId){
+    init(appId) {
         this.manager = new MidgarManager(appId)
         this.manager.start()
     }
 
-    trackScreen(screen){
+    trackScreen(screen) {
         this.manager.trackScreen(screen)
     }
 
-    killSwitch(){
+    killSwitch() {
         this.manager.stop()
     }
 }
