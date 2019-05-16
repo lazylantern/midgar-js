3183aa96816e16e227d0a351bd13f4ab650350f3
Guillaume Lachaud <guillaume@lazylantern.com>
2019-05-16 11:21:12 +0200

    ESLinting
    ```
    {
        "extends": "airbnb",
        "parser": "babel-eslint",
        "env": {
            "jest": true
        },
        "rules": {
            "no-use-before-define": "off",
            "react/jsx-filename-extension": "off",
            "react/prop-types": "off",
            "comma-dangle": "off"
        },
        "globals": {
            "fetch": false
        }
    }
    ```

--- midgar.js
+++ midgar.js
@@ -1,120 +1,119 @@
 class Event {
-    constructor(screen, source, type, date) {
-        this.screen = screen;
-        this.source = source;
-        this.type = type;
-        this.date = date;
-    }
+  constructor(screen, source, type, date) {
+    this.screen = screen;
+    this.source = source;
+    this.type = type;
+    this.date = date;
+  }
 }
 
 class MidgarApi {
-    static BASE_URL = "http://midgar-flask.herokuapp.com/api"
+    BASE_URL = 'http://midgar-flask.herokuapp.com/api';
 
     constructor(appId) {
-        this.appId = appId
+      this.appId = appId;
     }
 
     checkAppIsEnabled() {
-        return this.postRequest("/apps/kill", {
-            app_token: this.appId
-        })
+      return this.postRequest('/apps/kill', {
+        app_token: this.appId
+      });
     }
 
     uploadBatch(events) {
-        return this.postRequest("/events", {
-            app_token: this.appId,
-            events: events
-        })
+      return this.postRequest('/events', {
+        app_token: this.appId,
+        events
+      });
     }
 
     postRequest(endpoint, params) {
-        const url = MidgarApi.BASE_URL + endpoint
-        const body = JSON.stringify(params)
-        const headers = {
-            "Content-Type": "application/json; charset=UTF-8"
-        }
+      const url = this.BASE_URL + endpoint;
+      const body = JSON.stringify(params);
+      const headers = {
+        'Content-Type': 'application/json; charset=UTF-8'
+      };
 
-        return fetch(
-            url,
-            {
-                headers: headers,
-                body: body,
-                method: "POST"
-            }
-        )
+      return fetch(
+        url,
+        {
+          headers,
+          body,
+          method: 'POST'
+        }
+      );
     }
 }
 
 class MidgarManager {
+    static MAX_UPLOAD_BATCH_SIZE = 10;
 
-    static MAX_UPLOAD_BATCH_SIZE = 10
-    static UPLOAD_PERIOD_MS = 60 * 1000
+    static UPLOAD_PERIOD_MS = 60 * 1000;
 
     events = [];
+
     hasBeenRemotelyEnabled = false;
 
     constructor(appId) {
-        this.appId = appId
+      this.appId = appId;
     }
 
     start() {
-        this.api = new MidgarApi(this.appId)
-        this.api.checkAppIsEnabled().then(
-            response => {
-                if (response.ok) {
-                    this.hasBeenRemotelyEnabled = true
-                    this.timerId = setInterval(this.processBatch, MidgarManager.UPLOAD_PERIOD_MS)
-                }
-            }
-        )
+      this.api = new MidgarApi(this.appId);
+      this.api.checkAppIsEnabled().then(
+        (response) => {
+          if (response.ok) {
+            this.hasBeenRemotelyEnabled = true;
+            this.timerId = setInterval(this.processBatch, MidgarManager.UPLOAD_PERIOD_MS);
+          }
+        }
+      );
     }
 
     stop() {
-        this.hasBeenRemotelyEnabled = false
-        clearInterval(this.timerId)
-        this.events = []
+      this.hasBeenRemotelyEnabled = false;
+      clearInterval(this.timerId);
+      this.events = [];
     }
 
     trackScreen(screen) {
-        if (this.hasBeenRemotelyEnabled) {
-            this.events.push(this.createEvent(screen))
-        }
+      if (this.hasBeenRemotelyEnabled) {
+        this.events.push(this.createEvent(screen));
+      }
     }
 
     createEvent(screen) {
-        return new Event(screen, "js", "impression", new Date().getTime())
+      return new Event(screen, 'js', 'impression', new Date().getTime());
     }
 
     processBatch() {
-        var batch = this.events.splice(MidgarManager.MAX_UPLOAD_BATCH_SIZE)
-        this.api.uploadBatch(batch).then(
-            response => {
-                if (__DEV__) {
-                    if (response.ok) {
-                        console.log("Events successfully uploaded")
-                    } else {
-                        console.log("Something went wrong. Events got dropped.")
-                    }
-                }
+      const batch = this.events.splice(MidgarManager.MAX_UPLOAD_BATCH_SIZE);
+      this.api.uploadBatch(batch).then(
+        (response) => {
+          if (__DEV__) {
+            if (response.ok) {
+              console.log('Events successfully uploaded');
+            } else {
+              console.log('Something went wrong. Events got dropped.');
             }
-        )
+          }
+        }
+      );
     }
-
 }
 
 export default class MidgarTracker {
+  init(appId) {
+    this.manager = new MidgarManager(appId);
+    this.manager.start();
+    return this;
+  }
 
-    init(appId) {
-        this.manager = new MidgarManager(appId)
-        this.manager.start()
-        return this
-    }
+  trackScreen(screen) {
+    this.manager.trackScreen(screen);
+  }
 
-    trackScreen(screen) {
-        this.manager.trackScreen(screen)
-    }
-
-    killSwitch() {
-        this.manager.stop()
-    }
+  killSwitch() {
+    this.manager.stop();
+  }
 }
