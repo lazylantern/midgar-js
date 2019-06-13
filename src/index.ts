f549219725e1ad02e69a0555871675af27e6ce04
Guillaume Lachaud <guillaume@lazylantern.com>
2019-06-13 00:20:51 -0700

    Add HoC

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,7 @@
-import { NavigationState } from 'react-navigation';
-import MidgarManager from './manager';
+import Midgar from './midgar'
+import WithMidgar from './withMidgar'
 
-class MidgarTracker {
-    private manager?: MidgarManager;
-
-    public init(appId: string): void {
-        try {
-            this.manager = new MidgarManager(appId);
-            this.manager.start();
-        } catch (e) {
-            console.error(e);
-        }
-    }
-
-    public trackScreen(prevState: NavigationState, currentState: NavigationState): void {
-        try {
-            if (this.manager == null) {
-                return;
-            }
-            this.manager.trackScreenFromRoute(prevState, currentState);
-        } catch (e) {
-            console.error(e);
-        }
-    }
-
-    // track screen with given name
-    public manuallyTrackScreen(screen: string): void {
-        try {
-            if (this.manager == null) {
-                return;
-            }
-            this.manager.trackScreen(screen);
-        } catch (e) {
-            console.error(e);
-        }
-    }
-
-    public killSwitch(): void {
-        try {
-            if (this.manager == null) {
-                return;
-            }
-            this.manager.stop();
-        } catch (e) {
-            console.error(e);
-        }
-    }
+export {
+    Midgar,
+    WithMidgar,
 }
-
-export default new MidgarTracker();
---
+++ src/withMidgar.tsx
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import {NavigationContainer} from "react-navigation";
+import MidgarTracker from './midgar'
+
+const withExtendedContainer = <P extends object>(NavContainer: NavigationContainer): NavigationContainer => {
+    // @ts-ignore
+    return class WithMidgar extends NavContainer {
+
+        componentDidMount(): void {
+            let routeName = '';
+            //For react-navigation 2.x:
+            try {
+                // @ts-ignore
+                routeName = this.state.nav.routes[0].routeName
+            } catch (e) { }
+            //For react-navigation 3.x:
+            try {
+
+            } catch (e) { }
+            MidgarTracker.manuallyTrackScreen(routeName);
+        }
+
+        render() {
+            return super.render();
+        }
+    };
+};
+
+const withProps = <P extends object>(NavContainer: NavigationContainer) =>
+    class WithMidgar extends React.Component<P> {
+
+        render() {
+            return <NavContainer onNavigationStateChange={(prevState, currentState) => {
+                MidgarTracker.trackScreen(prevState, currentState);
+            }}/>;
+        }
+    };
+
+
+export default (WrappedComponent: NavigationContainer) =>
+    withProps(withExtendedContainer(WrappedComponent))
